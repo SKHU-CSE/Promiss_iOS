c5c559958b73e4bd66ea517ac021d97fa399c6be
Annab96 <lannab082@gmail.com>
2019-09-02 20:02:49 +0900

    project init

---
+++ Promiss_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Promiss_iOS
+//
+//  Created by Anna Lee on 02/09/2019.
+//  Copyright © 2019 Anna Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
