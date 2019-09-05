54650f437d48879914eb7932c53851e474ad7573
tngusmiso <k1miso012@gmail.com>
2019-09-05 17:15:14 +0900

    #5 [메인] 메인화면 레이아웃 구조 잡기 - 기능 미구현 - 지도 부분 미구현

---
+++ Promiss_iOS/Extension/UIColor+.swift
@@ -0,0 +1,32 @@
+//
+//  UIColor+.swift
+//  Promiss_iOS
+//
+//  Created by 임수현 on 04/09/2019.
+//  Copyright © 2019 Anna Lee. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var pmSeaBlue: UIColor {
+        return UIColor(red: 69 / 255, green: 79 / 255, blue: 161 / 255, alpha: 1.0)
+    }
+    
+    @nonobjc class var pmDarkBlue: UIColor {
+        return UIColor(red: 57 / 255, green: 65 / 255, blue: 128 / 255, alpha: 1.0)
+    }
+    
+    @nonobjc class var pmLemon: UIColor {
+        return UIColor(red: 1, green: 245 / 255, blue: 117 / 255, alpha: 1.0)
+    }
+    
+    @nonobjc class var pmIndiPink: UIColor {
+        return UIColor(red: 248 / 255, green: 105 / 255, blue: 110 / 255, alpha: 1.0)
+    }
+    
+    @nonobjc class var pmSkyBlue: UIColor {
+        return UIColor(red: 0 / 255, green: 176 / 255, blue: 240 / 255, alpha: 1.0)
+    }
+}
---
+++ Promiss_iOS/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  Promiss_iOS
+//
+//  Created by 임수현 on 04/09/2019.
+//  Copyright © 2019 Anna Lee. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var mapView: UIView!
+    @IBOutlet weak var titleView: UIView!
+    
+    @IBOutlet weak var profileButton: UIButton!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var logoLabel: UILabel!
+    
+    @IBOutlet weak var appointmentNameLabel: UILabel!
+    @IBOutlet weak var leftTimeLabel: UILabel!
+    
+    @IBOutlet weak var createOrDetailButton: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
