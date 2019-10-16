bb862a92db5f8d81e9cd0ae1215fd9c50a1b7145
tngusmiso <k1miso012@gmail.com>
2019-10-16 18:39:14 +0900

    #19 [회원가입] 비밀번호 조건 확인 - 비밀번호 조건 6~12자에서 벗어나면 안내문구 등장 - 비밀번호 불일치시 안내문구 등장 - 아이디 미입력 또는 안내문구 있을 시 버튼 비활성화

--- Promiss_iOS/Extension/UITextField+.swift
+++ Promiss_iOS/Extension/UITextField+.swift
@@ -13,4 +13,18 @@ extension UITextField {
         self.setAsWhiteBorderView()
         self.textColor = UIColor.white
     }
+    
+    func setPadding(left: CGFloat? = nil, right: CGFloat? = nil){
+        if let left = left {
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: left, height: self.frame.size.height))
+            self.leftView = paddingView
+            self.leftViewMode = .always
+        }
+        
+        if let right = right {
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: right, height: self.frame.size.height))
+            self.rightView = paddingView
+            self.rightViewMode = .always
+        }
+    }
 }
--- Promiss_iOS/SignupViewController.swift
+++ Promiss_iOS/SignupViewController.swift
@@ -16,7 +16,7 @@ class SignupViewController: UIViewController {
     
     @IBOutlet weak var idAlertLabel: UILabel!
     @IBOutlet weak var pwAlertLabel: UILabel!
-    @IBOutlet weak var pwCheckAlertLabel: UILabel!
+    @IBOutlet weak var checkPwAlertLabel: UILabel!
     
     @IBOutlet weak var signUpButton: UIButton!
     
@@ -35,30 +35,80 @@ class SignupViewController: UIViewController {
         self.dismiss(animated: true)
     }
     @IBAction func clickSignUpButton(_ sender: Any) {
+        // 회원가입 통신
+        // 성공 시 dismiss
+        // 실패 시 idAlertLabel 띄우기
         self.dismiss(animated: true)
     }
-    
 }
 
 extension SignupViewController{
     
     func setupCustomButton(){
         signUpButton.layer.cornerRadius = 6
+        signUpButton.setEnableFalse()
     }
     
     func setupCustomTextfield(textFields: UITextField...){
         for textField in textFields {
-            textField.layer.borderColor = UIColor.white.cgColor
-            textField.layer.borderWidth = 2
-            textField.layer.cornerRadius = 6
-            textField.textColor = UIColor.white
+            textField.setWhiteBorder()
+            textField.setPadding(left: 10, right: 10)
+            textField.addTarget(self, action: #selector(SignupViewController.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
         }
     }
     
     func setupAlertLabel(){
         idAlertLabel.isHidden = true
-        pwAlertLabel.isHidden = true
-        pwCheckAlertLabel.isHidden = true
+        pwAlertLabel.isHidden = false
+        checkPwAlertLabel.isHidden = true
     }
 }
 
+extension SignupViewController {
+    func isEnablePassword() -> Bool{
+        guard let pwText = pwTextField.text else {return false}
+        if pwText.count < 6 || pwText.count > 12 {
+            return false
+        }
+        return true
+    }
+    
+    func isSamePassword() -> Bool{
+        guard let pwText = pwTextField.text, let checkText = checkPwTextField.text else {return false}
+        
+        if pwText == checkText {
+            return true
+        }
+        return false
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if isEnablePassword() {
+            pwAlertLabel.isHidden = true
+        } else {
+            pwAlertLabel.isHidden = false
+        }
+        if isSamePassword() {
+            checkPwAlertLabel.isHidden = true
+        } else {
+            checkPwAlertLabel.isHidden = false
+        }
+        
+        if pwAlertLabel.isHidden && checkPwAlertLabel.isHidden && idTextField.text?.count != 0 {
+            signUpButton.setEnableTrue()
+        } else {
+            signUpButton.setEnableFalse()
+        }
+    }
+}
+
+extension UIButton {
+    func setEnableFalse() {
+        self.setTitleColor(UIColor.lightGray, for: .normal)
+        self.isEnabled = false
+    }
+    func setEnableTrue() {
+        self.setTitleColor(UIColor.black, for: .normal)
+        self.isEnabled = true
+    }
+}
