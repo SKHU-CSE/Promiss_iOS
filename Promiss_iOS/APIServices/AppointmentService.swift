ce34d0e5c8c092d520c55e87237848cae2483233
tngusmiso <k1miso012@gmail.com>
2019-10-18 11:28:11 +0900

    #23 [약속만들기] 약속 만들기 - 주소 선택은 default로 성공회대학교로 설정

---
+++ Promiss_iOS/APIServices/AppointmentService.swift
@@ -0,0 +1,9 @@
+//
+//  AppointmentService.swift
+//  Promiss_iOS
+//
+//  Created by 임수현 on 17/10/2019.
+//  Copyright © 2019 Anna Lee. All rights reserved.
+//
+
+import Foundation
---
+++ Promiss_iOS/Model/UserInfo.swift
@@ -0,0 +1,9 @@
+//
+//  UserInfo.swift
+//  Promiss_iOS
+//
+//  Created by 임수현 on 18/10/2019.
+//  Copyright © 2019 Anna Lee. All rights reserved.
+//
+
+import Foundation
